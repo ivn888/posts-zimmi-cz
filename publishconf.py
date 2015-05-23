c45d26798e8d340fc97d4ec2162633593767b288
Michal Zimmermann <zimmicz@gmail.com>
2015-05-23 13:17:30 +0200

    pelican settings

---
+++ publishconf.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+# This file is only used if you use `make publish` or
+# explicitly specify it as your config file.
+
+import os
+import sys
+sys.path.append(os.curdir)
+from pelicanconf import *
+
+SITEURL = 'http://www.zimmi.cz/posts'
+RELATIVE_URLS = False
+THEME = 'content/theme/simple'
+FEED_DOMAIN = SITEURL
+FEED_ALL_ATOM = 'atom.xml'
+CATEGORY_FEED_ATOM = None
+FEED_ALL_RSS = 'feed.xml'
+
+DELETE_OUTPUT_DIRECTORY = True
+
+# Following items are often useful when publishing
+
+#DISQUS_SITENAME = ""
+#GOOGLE_ANALYTICS = ""
+
+ARTICLE_URL = '{date:%Y}/{slug}/'
+ARTICLE_SAVE_AS = '{date:%Y}/{slug}/index.html'
